5d444cda074b8b5a2f715435f1e59beb2a908284
rbnograles <ryannograles.025.gtsi@gmail.com>
2022-09-23 20:25:28 +0800

    added validation for sign-in and signup

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,9 +1,21 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 export const router = express.Router();
 
-router.get('/sign-up',  (req, res) => { 
-    res.send("Hi there!");
+router.get('/sign-up', [
+        body('email')
+            .isEmail()
+            .withMessage("Email must be valid"),
+        body('password')
+            .trim()
+            .isLength({ min: 4, max: 20})
+            .withMessage("Password must be at least 4 characters short and at least 20 characters long")
+    ], 
+    (req: Request, res: Response) => { 
+    
+        const { email, password } = req.body;
+        
 });
 
 export { router  as signUpRouter};
