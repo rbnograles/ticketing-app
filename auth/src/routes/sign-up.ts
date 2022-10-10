1ee5d61a6c7bba09dc9e66ee24dbaf2c5afff5fa
rbnograles <ryannograles.025.gtsi@gmail.com>
2022-10-10 12:27:05 +0800

    Update: Validation position and move to a separate file

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,19 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import { EMAIL_VALIDATION, PASSWORD_VALIDATION } from '../middleware/validation';
 
 export const router = express.Router();
 
-router.get('/sign-up', [
-        body('email')
-            .isEmail()
-            .withMessage("Email must be valid"),
-        body('password')
-            .trim()
-            .isLength({ min: 4, max: 20})
-            .withMessage("Password must be at least 4 characters short and at least 20 characters long")
-    ], 
-    (req: Request, res: Response) => { 
-    
+router.get('/sign-up', [EMAIL_VALIDATION, PASSWORD_VALIDATION], (req: Request, res: Response) => {
         const { email, password } = req.body;
         
 });
