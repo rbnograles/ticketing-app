f77c000c3cd855b9782b2ab80f6892027992e072
Ryan Nograles <125443916+ryan-nograles@users.noreply.github.com>
2023-03-07 09:38:52 +0800

    added signup startup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,22 +6,42 @@ import { signUpRouter } from "./routes/sign-up";
 import { signInRouter } from "./routes/sign-in";
 import { signOutRouter } from "./routes/sign-out";
 import { errorHandler } from "./middleware/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+import mongoose from "mongoose";
 
 const server = express();
 const port = process.env.PORT || 3000;
 server.use(express.json({ limit: "50mb" }));
 
 // router consumers
-server.use("/api/users", currentUserRouter);
-server.use("/api/users", signUpRouter);
-server.use("/api/users", signInRouter);
-server.use("/api/users", signOutRouter);
+server.use("/api/auth", currentUserRouter);
+server.use("/api/auth", signUpRouter);
+server.use("/api/auth", signInRouter);
+server.use("/api/auth", signOutRouter);
+
+server.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
 
 server.use(errorHandler);
 
-// server start script
-server.listen(port, () => {
+const start = async () => {
+  try {
+    await mongoose.set("strictQuery", false);
+    // auth-mongo-srv is the domain name in auth auth-mongo-depl service
+    // af od mongoose v6 no options are needed
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to Mongodb");
+  } catch (error) {
+    console.log(error);
+  }
+
+  // listen for incoming traffic
+  server.listen(port, () => {
     console.clear();
     console.log("Auth Service is running successfully!");
     console.log("Service is listening on port " + port);
-});
+  });
+};
+
+start();
